21d00f2184c962daf13cb973f3ce0d4d75befa1b
Olamaryse <114025924+Olamaryse@users.noreply.github.com>
2023-04-13 13:16:10 +0100

    Set line endings to LF

---
+++ src/components/Home.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { HiArrowNarrowRight } from 'react-icons/hi';
+
+const Home = () => {
+  return (
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p className='text-pink-600'>Welcome to</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
+          Maria's Portfolio
+        </h1>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>
+          I’m a front-end developer specializing in 
+          designing exceptional digital experiences. Currently, I’m focused on
+          building responsive front-end web applications.
+        </p>
+        <div>
+          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+            View Work
+            <span className='group-hover:rotate-90 duration-300'>
+              <HiArrowNarrowRight className='ml-3 ' />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/data/data.js
@@ -0,0 +1,37 @@
+import Weather from '../assets/projects/4.png'
+import Quiz from '../assets/projects/2.png'
+import Music from '../assets/projects/3.png'
+import News from '../assets/projects/1.png'
+
+export const data=[
+    {
+        id:1,
+        name:"Weather Application",
+        image:Weather,
+        github:"https://github.com/Olamaryse/News-Catcher-new.git",
+        live:"https://not-your-regular-forecast.pages.dev/",
+    },
+    {
+        id:2,
+        name:"Quiz Application",
+        image:Quiz,
+        github:"https://github.com/Olamaryse/appquiz.git",
+        live:"https://appquiz.pages.dev/",
+    },
+    {
+        id:3,
+        name:"Music Application",
+        image:Music,
+        github:"https://github.com/Kaystringscode/MOODLY.git",
+        live:"https://lucky-tulumba-fcee3b.netlify.app/",
+    },
+    {
+        id:4,
+        name:"News Application",
+        image:News,
+        github:"https://github.com/Olamaryse/News-Catcher-new.git",
+        live:"https://news-catcher.pages.dev/",
+    },
+
+
+]
